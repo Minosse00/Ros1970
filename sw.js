54f4e9041440eb08534a757fcbff5dfce569b6ea
Minosse00 <semper2100@gmail.com>
2026-09-20 15:14:45 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,24 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('spese-auto-cache-v2').then((cache) => {
-      return cache.addAll([
-        './index.html',
-        './manifest.json',
-        './fiat.jpg'
-      ]);
-    })
-  );
-  self.skipWaiting();
-});
+const CACHE_NAME = 'ferie-app-v1';
+const ASSETS = [
+    './',
+    './index.html',
+    './manifest.json',
+    './icon-192.png',
+    './icon-512.png'
+];
 
-self.addEventListener('activate', (e) => {
-  e.waitUntil(clients.claim());
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS);
+        })
+    );
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
-  );
+    e.respondWith(
+        caches.match(e.request).then((response) => {
+            return response || fetch(e.request);
+        })
+    );
 });
